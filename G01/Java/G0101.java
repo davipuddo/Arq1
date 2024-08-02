97cd494db7d5e4b8eaa5c921fa3e8cdca6d75c4b
davipuddo <davifpuddo@gmail.com>
2024-08-02 17:35:54 -0300

    create repo

---
+++ G01/Java/G0101.java
@@ -0,0 +1,69 @@
+public class G0101
+{   
+    // Encontrar tamanho 
+    public static int FindSize (int x)
+    {
+        int result = 0;
+    
+        while (x > Math.pow(2, result))
+        {
+            if (result == 0)
+            {
+                result = 1;
+            }
+            else
+            {
+                result++;
+            }
+        }
+        return result;
+    }
+
+    // Converter inteiro para binario
+    public static byte[] dec2bin (int x)
+    {
+        byte [] result = null;
+        int size = FindSize(x);
+        result = new byte[size];
+        
+        int tmp = x;
+        for (int i = 0; i < size; i++)
+        {
+            if (tmp >= (int)Math.pow(2, (size -1) - i))
+            {
+                tmp = tmp - (int)Math.pow(2, (size -1) - i);
+                result[i] = 1;
+            }
+        }
+        return result;
+    }
+
+    public static void main (String[] args)
+    {   
+        // Definir valores
+        int value = 26;
+        int size = FindSize(value);
+
+        // Mostrar resultado
+        System.out.print(value + " = ");
+        for (int i = 0; i < size; i++)
+        {
+            byte [] res = dec2bin(value);
+            System.out.print(res[i]);
+        }
+        System.out.println("");
+
+        // Definir valores
+        value = 713;
+        size = FindSize(value);
+
+        // Mostrar resultado
+        System.out.print(value + " = ");
+        for (int i = 0; i < size; i++)
+        {
+            byte [] res = dec2bin(value);
+            System.out.print(res[i]);
+        }
+        System.out.println("");
+    }
+}
---
+++ G01/Java/G0102.java
@@ -0,0 +1,43 @@
+public class G0102 
+{   
+    // Converter inteiro para binario
+    public static int bin2dec (byte [] x)
+    {
+        int result = 0;
+        for (int i = 0; i < x.length; i++)
+        {
+            if (x[i] == 1)
+            {
+                result = result + (int)Math.pow(2, x.length-1-i);
+            }
+        }
+        return result;
+    }
+
+    public static void print (byte [] x)
+    {
+        for (int i = 0; i < x.length; i++)
+        {
+            System.out.print(x[i]);
+        }
+        int res = bin2dec(x);
+        System.out.print(" = " + res + "\n");
+    }
+
+    public static void main (String[] args)
+    {   
+        // Definir valores
+        byte[] a = {1,0,1,0,1};
+        byte[] b = {1,1,0,1,1};
+        byte[] c = {1,0,0,1,0};
+        byte[] d = {1,0,1,0,1,1};
+        byte[] e = {1,1,0,1,1,1};
+
+        // Mostrar resultado
+        print(a);
+        print(b);
+        print(c);
+        print(d);
+        print(e);
+    }
+}
